c04a5baf8a824edf9011c37cbe3580d0aa45f8ee
Moh'd Alrefai <malrefai@nyit.edu>
2017-04-12 21:56:38 +0300

    Fix renmae issues

--- ocpas/settings/production.py
+++ ocpas/settings/production.py
@@ -225,11 +225,3 @@ STATICFILES_FINDERS = (
 )
 
 ALDRYN_BOILERPLATE_NAME = 'bootstrap3'
-
-import dj_database_url
-
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
