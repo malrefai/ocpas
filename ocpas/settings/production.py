9e77ed3d3fe27eceb227de61b85321c1396ab5cc
Moh'd Alrefai <malrefai@nyit.edu>
2017-04-12 17:34:12 +0300

    Fix static name

--- ocpas/settings/production.py
+++ ocpas/settings/production.py
@@ -58,7 +58,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static-serve/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
